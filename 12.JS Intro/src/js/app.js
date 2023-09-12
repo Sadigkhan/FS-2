507acb372dd14afda4fef4c6e5d19589fc9d0f0c
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-12 13:56:51 +0400

    Js Intro

---
+++ 12.JS Intro/src/js/app.js
@@ -0,0 +1,144 @@
+// console.log("Hello World");
+// console.warn("Hello World");
+// console.error("Hello World");
+
+
+//Variables & DataTypes
+
+// var ad = "John Doe";
+// console.log(ad);
+// ad="Vidadi Sahmarov"
+// console.log(ad);
+
+// const ad = "John Doe";
+// ad="Vidadi Sahmarov"
+// console.log(ad);
+
+
+//Primitive DataTypes
+//String
+// let ad = "John Doe";
+// console.log(typeof ad);
+// //Number
+// let yas = 30;
+// console.log(typeof yas);
+// //Boolean
+// let evli = true;
+// console.log(typeof evli);
+// //Null
+// let bos = null;
+// console.log(typeof bos);
+// //Undefined
+// let tanimsiz;
+// console.log(typeof tanimsiz);
+
+//Reference DataTypes Non Primitive dataTypes
+//Objects
+// var array = ["John", "Jane", "Mark","Bob"];
+// console.log( array[array.length - 1]);
+// console.log(typeof array);
+// let person = {
+//     ad: "John",
+//     soyad: "Doe",
+//     yas: 22,
+//     evli: true,
+//     seher: "Baki",
+//     birthDate: function(){
+//         return new Date().getFullYear() - this.yas;
+//     }
+// }
+
+
+// console.log(person.ad + " " + person.soyad + " " + person.birthDate());
+
+
+// let lamp=document.getElementById("lamp");
+// function LightMode(){
+//     // document.body.style.backgroundColor = "white";
+//     // document.body.style.color = "black";
+//     lamp.style.filter="grayscale(0)"
+
+
+// }
+
+// function DarkMode(){
+//     // document.body.style.backgroundColor = "black";
+//     // document.body.style.color = "white";
+//     lamp.style.filter="grayscale(1)"
+
+// }
+
+// console.log(typeof(typeof(null)));
+
+
+// alert("Hello World");
+// prompt("Adinizi daxil edin");
+
+// confirm();
+//print to screen
+// document.getElementById("user-name").innerHTML = "<i>Vidadi</i>";
+// document.getElementById("username").innerText = "<i>Vidadi</i>";
+
+
+//operators
+//  let a = 10;
+//  let b = "10";
+
+//  console.log(a===b);
+
+
+// let pi= Math.PI;
+
+
+
+
+// function RandomNumber(){
+// let h1 = document.getElementById("num");
+
+//     h1.innerHTML += Math.floor(Math.random() * 100) + "<br>";
+// }
+
+
+// !=
+// !==
+
+
+// let num=5;
+
+// if(num!="5"){
+//     alert("hi")
+// }
+// else{
+//     alert("bye")
+// }
+
+
+
+// if(num=="5"){
+//     alert("hi")
+// }
+// else{
+//     alert("bye")
+// }
+//turnary operator
+//  num==5 ? alert("hi") : alert("bye");
+
+
+
+// var date = new Date().get();
+// console.log(date);
+
+// if(date>=6 && date<12) {
+//     alert('Good Morning');
+// }
+// else if(date>=12 && date<18) {
+//     alert('Good Afternoon');
+// }
+// else if(date>=18 && date<6){
+//     alert('Good Night');
+// }
+
+
+// var array = [[1,2,3,4],[1,2,3,4,5,6]];
+
+// console.log(array[1][5]);
