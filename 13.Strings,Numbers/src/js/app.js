ec90a23a4c373495e7c2de0152f8935b63f8fb97
Sadigkhan Gaykhanov <sadigkhaneg@code.edu.az>
2023-09-13 12:58:08 +0400

    String,Numbers,Array

---
+++ 13.Strings,Numbers/src/js/app.js
@@ -0,0 +1,149 @@
+var text="sarifrr mArif elnur arif";
+// var text2= 'Sagol';
+// var newText="Hello world";
+// var text3=new String("Hello World");
+
+// console.log(typeof text);
+// console.log(typeof text2);
+// console.log(typeof text3);
+
+// var person={
+//     name:"Elvin",
+// }
+
+// console.log(person.name);   
+
+// console.log(text.length)
+// console.log(text.toUpperCase())
+// console.log(text.toLowerCase())
+// console.log(text.charAt(0))
+// console.log(text.charAt(text.length-1))
+// console.log(text.indexOf("r"))
+// console.log(text.indexOf("r",1))
+// console.log(text.indexOf("r",text.indexOf("r")+1))
+// console.log(text.lastIndexOf("r"))
+// console.log(text.includes("arif"))
+// console.log(text.includes("arif",1))
+// console.log(text.startsWith("arif"))
+// console.log(text.endsWith("arif"))
+// console.log(text.slice(0,4))
+// console.log(text.slice(2))
+// console.log(text.slice(-10))
+// console.log(text.split(" "))
+// console.log(text.replace("arif","Elvin"))
+// console.log(text.replace(/arif/gi,"Elvin"))
+
+// var text="Elvin";
+// var text2="Elizade";
+// var text3="Elvin Elizade";
+
+// console.log((text.concat("-",text2)).concat("-",text3))
+
+// let text1 = "      Hello World!      ";
+// let text2 = text1.trim("");
+
+// console.log(text2);
+
+
+// console.log(text.charCodeAt(0))
+
+
+
+//Number Methods
+
+// var num=1;
+// var  num2=1.2;
+// var num3 =new Number(1);
+// var num =10.5920123123;
+// console.log(num)
+// console.log(num.toString())
+
+// console.log(num.toFixed(3))
+// console.log(num.toPrecision(3))
+// console.log(num.toExponential(3))
+
+// Number(true);
+// Number(false);
+// Number("10");
+// Number("  10");
+// Number("10  ");
+// Number(" 10  ");
+// Number("10.33");
+// Number("10,33");
+// Number("10 33");
+// Number("John");
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("-10"));
+// console.log(Number("  10"));
+// console.log(Number("10  "));
+// console.log(Number(" 10  "));
+// console.log(Number("10.33"));
+// console.log(Number("10,33"));
+// console.log(Number("10 33"));
+// console.log(Number("John"));
+
+
+
+// console.log(parseInt("-10"));
+// console.log(parseInt("-10.33"));
+// console.log(parseInt("10"));
+// console.log(parseInt("10.33"));
+// console.log(parseInt("10 20 30"));
+// console.log(parseInt("10 years"));
+// console.log(parseInt(" years 10"));
+
+
+// console.log(parseFloat("-10"));
+// console.log(parseFloat("-10.33"));
+// console.log(parseFloat("10"));
+// console.log(parseFloat("10.33"));
+// console.log(parseFloat("10 20 30"));
+// console.log(parseFloat("10 years"));
+// console.log(parseFloat("1years 10"));
+
+
+// var num1=10; 
+// console.log(Number.isInteger("asdasd"))      
+
+let numbers=[1,2,3,4,5,6,7,8,9,10,];
+// let numbers2=new Array(1,2,3,4,5,6,7,8,9,10);
+// console.log( numbers)
+// console.log( numbers2)
+
+// const cars = ["Saab","Volvo","BMW"];
+
+//   console.log(cars)
+
+
+//   let nums3= new Array();
+//   nums3[0]=1;
+//     nums3[1]=2;
+//     nums3[2]=3;
+//     nums3[6]=4;
+
+//     console.log(nums3[3])
+
+// numbers.pop();
+// console.log(numbers)
+// numbers.push(11);
+// console.log(numbers)
+// numbers.shift();
+// console.log(numbers)
+// numbers.unshift(1);
+// console.log(numbers)
+// let anotherNumbers=["demo",numbers];
+// console.log(numbers.join("-"))
+
+// delete numbers[0];
+// console.log(numbers)
+// console.log(numbers.length)
+// console.log(typeof numbers[0])
+
+var words=["asd","asda","adasd"];
+numbers=numbers.concat(words);
+console.log(numbers)
+
+
+
